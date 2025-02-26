c5f7a5af95c7592b83a6bdbe6ccae93f965451ba
Haris Mughal <harismirza3456@gmail.com>
2025-02-27 00:44:45 +0500

    fixed download cv button

--- src/components/DownloadCVButton/DownloadCVButton.tsx
+++ src/components/DownloadCVButton/DownloadCVButton.tsx
@@ -1,9 +1,7 @@
-// import {useDownloadPdf} from 'hooks/useDownloadPdf';
 import {useMediaQuery} from 'react-responsive';
 import {mediaBreakpoints} from 'responsive';
 
 export const DownloadCVButton = ({isSmall}: {isSmall?: boolean}) => {
-  // const [, handleDownload] = useDownloadPdf(import.meta.env.VITE_CV_PATH, 'Haris-Developer-CV.pdf');
 
   const mobile = useMediaQuery({
     query: `(max-width: ${mediaBreakpoints.sm}px)`,
@@ -11,19 +9,17 @@ export const DownloadCVButton = ({isSmall}: {isSmall?: boolean}) => {
 
   return mobile ? (
     <a
-      href="/cv.pdf"
-      download="Haris-Developer-CV.pdf"
+      href="/harisDeveloperCV.pdf"
+      download="harisDeveloperCV.pdf"
       className={`${!isSmall ? 'mb-8 mr-10 w-full rounded px-12 py-4 text-center sm:mb-0 sm:w-fit' : ''}  btn`}
-      target="_blank"
     >
       Download CV
     </a>
   ) : (
     <a
-      href="/cv.pdf"
-      download="Haris-Developer-CV.pdf"
+      href="/harisDeveloperCV.pdf"
+      download="harisDeveloperCV.pdf"
       className={`${!isSmall ? 'mb-8 mr-10 w-full rounded px-12 py-4 sm:mb-0 sm:w-fit' : ''}  btn`}
-      target="_blank"
     >
       Download CV
     </a>
